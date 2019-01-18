d67d097ecf793f8396d9a8772940813293bbe1b4
Taaseen <tali2@stuy.edu>
2019-01-18 11:20:43 -0500

    skjfl

--- dashboard.c
+++ dashboard.c
@@ -12,29 +12,29 @@ void add_friend(int client_sockfd, struct user usr){
   char string[BUFFER_SIZE];
   struct user friend;
   recv(client_sockfd, string, BUFFER_SIZE,0);
-  int unreg = lookup_acount(string, &friend);
+  int unreg = lookup_account(string, &friend);
   if(unreg){
     printf("Friend add fail, user doesn't exist\n");
     send(client_sockfd, ERR, BUFFER_SIZE,0);
-    return;
+    return; 
   }
 
   printf("Sending request %s\n", friend.name);
   send(client_sockfd, PASS, BUFFER_SIZE,0);
   
-  friend.requests[friend.num_requests] = usr.name;
+  strcpy(&friend.requests[friend.num_requests], usr.name);
   friend.num_requests++;
   
 }
 
-void dashboard_main(int client_sockfd){
+void dashboard_main(int client_sockfd, struct user usr){
   int logged_in = 1;
   char string[BUFFER_SIZE];
   
   while(logged_in){
     recv(client_sockfd, string, BUFFER_SIZE,0);
     if(!strncmp(string, "add friend", 10)){
-      add_friend(client_sockfd);p
+      add_friend(client_sockfd, usr);
     }
   }
 }
